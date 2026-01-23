20e65dbd5d33d6c4d15a800ec2fce9e4b6508833
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-01-23 09:40:32 +0100

    first dummy prompt

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World")
+import { OpenRouter } from "@openrouter/sdk";
+
+const client = new OpenRouter({
+    apiKey: process.env.HACKCLUB_API_KEY,
+    serverURL: process.env.HACKCLUB_URL,
+});
+
+const response = await client.chat.send({
+    model: "qwen/qwen3-32b",
+    messages: [
+        {
+            role: "user",
+            content: "Tell me a joke",
+        }
+    ],
+    stream: false,
+});
+
+console.log(response.choices[0]?.message.content);
